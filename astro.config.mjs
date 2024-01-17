4296b07a5877cc0a3e30b3f5bf5ee4190aae5f75
Pawan Gawande <129038360+ppgawande12@users.noreply.github.com>
2024-01-17 16:24:23 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-import vercel from "@astrojs/vercel/serverless";
 
-import node from "@astrojs/node";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  integrations: [react()]
 });
